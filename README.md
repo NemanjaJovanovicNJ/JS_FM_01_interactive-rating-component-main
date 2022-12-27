fce171538361e3eb115a6fa736cdc74bca2a1101
NemanjaJovanovicNJ <jovanovic.p.nemanja@gmail.com>
2022-12-27 21:52:52 +0100

    Readme updated

---
+++ README.md
@@ -0,0 +1,31 @@
+# Frontend Mentor - Interactive Rating Component
+
+---
+
+## Table of Content
+
+* [Useful Links](#useful-links)
+* [Mobile version](#mobile)
+* [Desktop version](#desktop)
+
+---
+
+### Useful Links <a name="useful-links"></a>
+
+* [FM - Interactive Rating Component](https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI)
+
+* [Vercel: My Solution](https://js-fm-01-interactive-rating-component-main.vercel.app/)
+
+--- 
+### Mobile version <a name="mobile"></a>
+![Design preview for the QR code component coding challenge](./Screenshot/mobile.png)
+
+--- 
+### Desktop version <a name="desktop"></a>
+![Design preview for the QR code component coding challenge](./Screenshot/desktop.png)
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -8,17 +8,16 @@ const rate4 = document.getElementById("4");
 const rate5 = document.getElementById("5");
 const ratingMessage = document.querySelector(".rating-message");
 const rates = [rate1, rate2, rate3, rate4, rate5];
-console.log(rates);
 
 let currentRating;
-
+// Function for reseting all buttons in a case when previously any of the buttons is already clicked
 const resetRates = function () {
   for (let i = 0; i < rates.length; i++) {
     rates[i].style.backgroundColor = "hsl(213, 19%, 18%)";
     rates[i].style.color = "hsl(217, 12%, 63%)";
   }
 };
-// SUBMIT Button - Only left is to change rate value in thank you container
+// SUBMIT Button
 
 submitBtnEl.addEventListener("click", function () {
   document.querySelector(".container-rate").classList.add("hidden");
@@ -26,35 +25,11 @@ submitBtnEl.addEventListener("click", function () {
   ratingMessage.textContent = `You selected ${currentRating} out of 5`;
 });
 
-// Come back later and solve this using "forEach" method probably - according to web research
-
-rate1.addEventListener("click", function () {
-  resetRates();
-  rate1.style.backgroundColor = " hsl(217, 12%, 63%)";
-  rate1.style.color = "#fff";
-  currentRating = rate1.textContent;
-});
-rate2.addEventListener("click", function () {
-  resetRates();
-  rate2.style.backgroundColor = " hsl(217, 12%, 63%)";
-  rate2.style.color = "#fff";
-  currentRating = rate2.textContent;
-});
-rate3.addEventListener("click", function () {
-  resetRates();
-  rate3.style.backgroundColor = " hsl(217, 12%, 63%)";
-  rate3.style.color = "#fff";
-  currentRating = rate3.textContent;
-});
-rate4.addEventListener("click", function () {
-  resetRates();
-  rate4.style.backgroundColor = " hsl(217, 12%, 63%)";
-  rate4.style.color = "#fff";
-  currentRating = rate4.textContent;
-});
-rate5.addEventListener("click", function () {
-  resetRates();
-  rate5.style.backgroundColor = " hsl(217, 12%, 63%)";
-  rate5.style.color = "#fff";
-  currentRating = rate5.textContent;
-});
+for (let i = 0; i < rates.length; i++) {
+  rates[i].addEventListener("click", function () {
+    resetRates();
+    rates[i].style.backgroundColor = " hsl(217, 12%, 63%)";
+    rates[i].style.color = "#fff";
+    currentRating = rates[i].textContent;
+  });
+}
